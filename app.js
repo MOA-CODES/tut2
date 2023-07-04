d459d7745f7f0a13449c29e3522bfd22899d52ee
MOA -CODES <moyo.ajla@gmail.com>
2023-07-04 16:40:09 +0100

    setting up with just http module example

--- app.js
+++ app.js
@@ -2,25 +2,48 @@ const{readFileSync} = require('fs');
 const http = require('http');
 
 //get all files
-const homepage = readFileSync('./index.html');
+const homepage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
+
 
 const server = http.createServer((req, res) => {
-const url = req.url;
-if(url == '/') {
-    res.writeHead(200, {'content-type':'text/html'} )
-    res.write(homepage)
-    res.end()
-}
-else if(url == '/about') {
-    res.writeHead(200, {'content-type':'text/html'} )
-    res.write('<h1>about page</h1>')
-    res.end()
-}
-else {
-    res.writeHead(404, {'content-type':'text/html'} )
-    res.write('<h1>page not found</h1>')
-    res.end();
-}
+    const url = req.url;
+    console.log(url);
+        if(url == '/') {
+            res.writeHead(200, {'content-type':'text/html'} )
+            res.write(homepage)
+            res.end()
+        }
+        //styles
+        else if(url == '/styles.css') {
+            res.writeHead(200, {'content-type':'text/css'} )
+            res.write(homeStyles)
+            res.end()
+        }
+        //logic
+        else if(url == '/browser-app.js') {
+            res.writeHead(200, {'content-type':'text/javascript'} )
+            res.write(homeLogic)
+            res.end()
+        }
+        //logo
+        else if(url == '/logo.svg') {
+            res.writeHead(200, {'content-type':'image/svg+xml'} )
+            res.write(homeImage)
+            res.end()
+        }
+        else if(url == '/about') {
+            res.writeHead(200, {'content-type':'text/html'} )
+            res.write('<h1>about page</h1>')
+            res.end()
+        }
+        else {
+            res.writeHead(404, {'content-type':'text/html'} )
+            res.write('<h1>page not found</h1>')
+            res.end();
+        }
 
 })
 
