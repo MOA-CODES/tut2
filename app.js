10dc194aa9ac93be2f40e539ca2f141fa86d93bb
MOA -CODES <moyo.ajla@gmail.com>
2023-07-09 19:40:57 +0100

    grouping & making app.js simpler with routes

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 const express = require('express');
 const app = express();
-const logger = require('./logger');
-const authorize = require('./authorize');
-const morgan = require('morgan');//third party
 
-// req=> middleware => res
-//app.use(logger)//instead of placing in each app.get/app.out.etc
-//app.use('/api',logger)//it can be applied to only specific routes
+const peopleRouter = require('./routes/people');
+const authRouter = require('./routes/auth');
 
-//app.use([logger, authorize])//you can place multiple middlewares by using an array of middlewares
-app.use(morgan('tiny'))
-//app.use(logger)
-//app.use(express.static('./public'))
+//static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended:false}))//middleware use to get data
+//parse json data
+app.use(express.json()) //helps with access to js data
 
-app.get('/',(req, res)=>{
-    res.send('Home')
-})
-app.get('/about',(req, res)=>{
-    res.send('About')
-})
-app.get('/api/products',(req, res)=>{
-    res.send('Api Products')
-})
-app.get('/api/items',authorize, (req, res)=>{
-    console.log(req.user)
-    res.send('Api items')
-})
-
-
+app.use('/api/people', peopleRouter)
+app.use('/login', authRouter)
 
 app.listen(5000,()=>{
     console.log('server is listening on port 5000..');
